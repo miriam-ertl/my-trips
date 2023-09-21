87a82953427b7f8bcae4804dcc8aa47a23bec238
David Krauss <davey.krauss@googlemail.com>
2023-09-21 13:10:18 +0200

    feat: add TripForm and test on homepage

---
+++ components/TripForm/index.js
@@ -0,0 +1,87 @@
+import useSWR from "swr";
+
+export default function TripForm() {
+  const { mutate } = useSWR("/api/trips");
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const tripData = Object.fromEntries(formData);
+    const response = await fetch("/api/trips", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(tripData),
+    });
+    if (!response.ok) {
+      console.error(response.status);
+      return;
+    }
+    mutate();
+    event.target.reset();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <label htmlFor="title">Title (max. 30 characters)*</label>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          maxlength="30"
+          required
+        ></input>
+
+        <label htmlFor="startDate">Starting date (dd/mm/yyyy)*</label>
+        <input id="startDate" name="startDate" type="date" required></input>
+
+        <label htmlFor="endDate">Ending date (dd/mm/yyyy)*</label>
+        <input id="endDate" name="endDate" type="date" required></input>
+
+        <label htmlFor="city">City (max. 30 characters)*</label>
+        <input
+          id="city"
+          name="city"
+          type="text"
+          maxlength="30"
+          required
+        ></input>
+
+        <label htmlFor="country">Country (max. 30 characters)*</label>
+        <input
+          id="country"
+          name="country"
+          type="text"
+          maxlength="30"
+          required
+        ></input>
+
+        <label htmlFor="image">Image (URL)*</label>
+        <input
+          id="image"
+          name="image"
+          type="text"
+          placeholder="For example www.my-image.com"
+          required
+        ></input>
+
+        <label htmlFor="description">Description (max. 250 characters)*</label>
+        <textarea
+          rows="8"
+          cols="30"
+          maxlength="10"
+          id="description"
+          name="description"
+          type="text"
+          placeholder="Enter your description"
+        ></textarea>
+        <p>
+          <span>10</span> characters left
+        </p>
+        <button type="submit"> + Add Trip</button>
+      </fieldset>
+    </form>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import TripList from "@/components/TripList";
+import TripForm from "@/components/TripForm";
 
 export default function HomePage() {
   return (
     <div>
       <h1>My Trips</h1>
+      <TripForm />
       <TripList />
     </div>
   );
