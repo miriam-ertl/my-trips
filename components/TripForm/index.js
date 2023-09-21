b11d1f5e96d274dc38840dfa0e05c375379289a5
David Krauss <davey.krauss@googlemail.com>
2023-09-21 18:43:27 +0200

    feat: add ConfirmationMessage component

--- components/TripForm/index.js
+++ components/TripForm/index.js
@@ -1,6 +1,6 @@
 import useSWR from "swr";
-import Link from "next/link";
 import { useRouter } from "next/router";
+import ConfirmationMessage from "../ConfirmationMessage";
 
 export default function TripForm() {
   const { mutate } = useSWR("/api/trips");
@@ -29,68 +29,74 @@ export default function TripForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <fieldset>
-        <label htmlFor="title">Title (max. 30 characters)*</label>
-        <input
-          id="title"
-          name="title"
-          type="text"
-          maxLength="30"
-          required
-        ></input>
+    <>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="title">Title (max. 30 characters)*</label>
+          <input
+            id="title"
+            name="title"
+            type="text"
+            maxLength="30"
+            required
+          ></input>
 
-        <label htmlFor="startDate">Starting date (dd/mm/yyyy)*</label>
-        <input id="startDate" name="startDate" type="date" required></input>
+          <label htmlFor="startDate">Starting date (dd/mm/yyyy)*</label>
+          <input id="startDate" name="startDate" type="date" required></input>
 
-        <label htmlFor="endDate">Ending date (dd/mm/yyyy)*</label>
-        <input id="endDate" name="endDate" type="date" required></input>
+          <label htmlFor="endDate">Ending date (dd/mm/yyyy)*</label>
+          <input id="endDate" name="endDate" type="date" required></input>
 
-        <label htmlFor="city">City (max. 30 characters)*</label>
-        <input
-          id="city"
-          name="city"
-          type="text"
-          maxLength="30"
-          required
-        ></input>
+          <label htmlFor="city">City (max. 30 characters)*</label>
+          <input
+            id="city"
+            name="city"
+            type="text"
+            maxLength="30"
+            required
+          ></input>
 
-        <label htmlFor="country">Country (max. 30 characters)*</label>
-        <input
-          id="country"
-          name="country"
-          type="text"
-          maxLength="30"
-          required
-        ></input>
+          <label htmlFor="country">Country (max. 30 characters)*</label>
+          <input
+            id="country"
+            name="country"
+            type="text"
+            maxLength="30"
+            required
+          ></input>
 
-        <label htmlFor="image">Image (URL)*</label>
-        <input
-          id="image"
-          name="image"
-          type="text"
-          placeholder="For example www.my-image.com"
-          required
-        ></input>
+          <label htmlFor="image">Image (URL)*</label>
+          <input
+            id="image"
+            name="image"
+            type="text"
+            placeholder="For example www.my-image.com"
+            required
+          ></input>
 
-        <label htmlFor="description">Description (max. 150 characters)*</label>
-        <textarea
-          rows="8"
-          cols="30"
-          maxLength="150"
-          id="description"
-          name="description"
-          type="text"
-          required
-          placeholder="Enter your description"
-        ></textarea>
-        <p>
-          <span>150</span> characters left
-        </p>
+          <label htmlFor="description">
+            Description (max. 150 characters)*
+          </label>
+          <textarea
+            rows="8"
+            cols="30"
+            maxLength="150"
+            id="description"
+            name="description"
+            type="text"
+            required
+            placeholder="Enter your description"
+          ></textarea>
+          <p>
+            <span>150</span> characters left
+          </p>
 
-        <button type="submit"> + Add Trip</button>
-      </fieldset>
-      <p>* required form field</p>
-    </form>
+          <button type="submit"> + Add Trip</button>
+        </fieldset>
+        <p>* required form field</p>
+      </form>
+
+      <ConfirmationMessage button={"Cancel"} />
+    </>
   );
 }
--- pages/addTrip.js
+++ pages/addTrip.js
@@ -1,9 +1,11 @@
 import TripForm from "@/components/TripForm";
 import Link from "next/link";
+import ConfirmationMessage from "@/components/ConfirmationMessage";
+
 export default function AddTrip() {
   return (
     <main>
-      <Link href="/">&larr;</Link>
+      <ConfirmationMessage button="&larr;" />
       <h1>My Trips</h1>
       <h2>Add a trip</h2>
       <TripForm />
