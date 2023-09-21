f0a43dae3bdc3a98487726c5382e32c33412d662
David Krauss <davey.krauss@googlemail.com>
2023-09-21 15:56:23 +0200

    refactor: add html structure to addTrip.js and a backlink

--- pages/addTrip.js
+++ pages/addTrip.js
@@ -1,4 +1,12 @@
 import TripForm from "@/components/TripForm";
+import Link from "next/link";
 export default function AddTrip() {
-  return <TripForm />;
+  return (
+    <main>
+      <Link href="/">&larr;</Link>
+      <h1>My Trips</h1>
+      <h2>Add a trip</h2>
+      <TripForm />
+    </main>
+  );
 }
