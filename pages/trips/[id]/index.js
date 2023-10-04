b77c4c629ea22a2c99708973a24ec935fcae8a02
Miriam Ertl <miriam_ertl@web.de>
2023-10-04 17:03:56 +0200

    feat: create handle function for complete and add return for complete

--- pages/trips/[id]/index.js
+++ pages/trips/[id]/index.js
@@ -13,10 +13,11 @@ export default function DetailsPage() {
     isLoading,
     mutate,
   } = useSWR(id ? `/api/trips/${id}` : null);
+
   if (!trip || isLoading) {
     return "... is loading";
   }
-
+  console.log(trip);
   async function handleDeleteTrip() {
     await fetch(`/api/trips/${id}`, {
       method: "DELETE",
@@ -44,9 +45,69 @@ export default function DetailsPage() {
 
     mutate();
   }
+
+  async function handleDeleteFromPackingList(_id) {
+    const residualItems = trip.packingList.filter(
+      (deleteItem) => deleteItem._id !== _id
+    );
+    const updatedTrip = {
+      ...trip,
+      packingList: [...residualItems],
+    };
+
+    const response = await fetch(`/api/trips/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTrip),
+    });
+
+    if (!response.ok) {
+      alert("Error updating trip");
+      return;
+    }
+
+    mutate();
+  }
+
+  async function handleCompletePackingList(_id) {
+    const completeItems = trip.packingList.map((completeItem) =>
+      completeItem._id === _id
+        ? { ...completeItem, checked: !completeItem.checked }
+        : { ...completeItem }
+    );
+    const updatedTrip = {
+      ...trip,
+      packingList: [...completeItems],
+    };
+    console.log(updatedTrip);
+
+    const response = await fetch(`/api/trips/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTrip),
+    });
+
+    if (!response.ok) {
+      console.error(response.status);
+      return;
+    }
+
+    mutate();
+  }
+  const packedListItems = trip.packingList.filter(
+    (listItem) => listItem.checked
+  );
+  const unpackedListItems = trip.packingList.filter(
+    (listItem) => !listItem.checked
+  );
+
   return (
     <main>
-      <Link href="/" aria-label="Go back to hompage">
+      <Link href="/" aria-label="Go back to homepage">
         &larr;
       </Link>
       <ConfirmDelete handleDeleteTrip={handleDeleteTrip} />
@@ -54,7 +115,6 @@ export default function DetailsPage() {
       <Link href="#packingList" type="button">
         <button>go to packing list</button>
       </Link>
-
       <h1>My Trips</h1>
       <section>
         <Image
@@ -67,39 +127,71 @@ export default function DetailsPage() {
         {trip.city}, {trip.country}
         {trip.startDate} - {trip.endDate}
         <h3>My plans</h3>
-        <p>
-          {trip.description}
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-          amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-          At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
-          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-          amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-          diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-          erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-          et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-          Lorem ipsum dolor sit amet.
-        </p>
+        <p>{trip.description}</p>
         <h3 id="packingList">{trip.title} packing list:</h3>
         <PackingListForm onHandleAddToPackingList={handleAddToPackingList} />
         {trip.packingList.length === 0 ? (
           <p>
-            Your packing list is empty. <br></br> Do you want to add something?
+            Your packing list is empty.<br></br> Do you want to add something?
           </p>
         ) : (
-          <ul>
-            {trip.packingList.map(({ _id, name }) => (
-              <li key={_id}>{name}</li>
-            ))}
-          </ul>
+          <>
+            <ul>
+              {unpackedListItems.map(({ _id, name, checked }) => (
+                <li key={_id}>
+                  <input
+                    type="checkbox"
+                    defaultChecked={false}
+                    checked={checked}
+                    onChange={() => handleCompletePackingList(_id)}
+                  />
+                  <span
+                    style={{
+                      textDecoration: checked ? "line-through" : "none",
+                    }}
+                  >
+                    {name}
+                  </span>
+                  <button
+                    type="button"
+                    onClick={() => handleDeleteFromPackingList(_id)}
+                  >
+                    &#10060;
+                  </button>
+                </li>
+              ))}
+            </ul>
+            {packedListItems.length > 0 && (
+              <>
+                <h3>Done:</h3>
+                <ul>
+                  {packedListItems.map(({ _id, name, checked }) => (
+                    <li key={_id}>
+                      <input
+                        type="checkbox"
+                        defaultChecked={false}
+                        checked={checked}
+                        onChange={() => handleCompletePackingList(_id)}
+                      />
+                      <span
+                        style={{
+                          textDecoration: checked ? "line-through" : "none",
+                        }}
+                      >
+                        {name}
+                      </span>
+                      <button
+                        type="button"
+                        onClick={() => handleDeleteFromPackingList(_id)}
+                      >
+                        &#10060;
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
+          </>
         )}
       </section>
     </main>
