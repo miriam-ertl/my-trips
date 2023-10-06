977d5493692f782a4cd09a2d038e21891c33e71c
Bonnie Baumeister <bonnie.hoika@gmx.de>
2023-10-06 14:33:43 +0200

    feat: add Tripform.styled.js and style addTrip form

---
+++ components/TripForm/TripForm.styled.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const StyledFormAddTrip = styled.div`
+  display: grid;
+`;
+
+export const StyledTitleAddTrip = styled.div`
+  display: grid;
+`;
+export const StyledStartDateAddTrip = styled.div`
+  display: grid;
+  justify-content: stretch;
+`;
+export const StyledEndDateAddTrip = styled.div`
+  display: grid;
+  justify-content: stretch;
+`;
+
+export const StyledCityAddTrip = styled.div`
+  display: grid;
+  justify-content: stretch;
+`;
+
+export const StyledCountryAddTrip = styled.div`
+  display: grid;
+  justify-content: stretch;
+`;
+
+export const StyledImageAddTrip = styled.div`
+  display: grid;
+  justify-content: stretch;
+`;
+export const StyledDescriptionAddTrip = styled.div`
+  display: grid;
+  justify-content: stretch;
+`;
--- components/TripForm/index.js
+++ components/TripForm/index.js
@@ -1,3 +1,14 @@
+import {
+  StyledCityAddTrip,
+  StyledCountryAddTrip,
+  StyledDescriptionAddTrip,
+  StyledEndDateAddTrip,
+  StyledFormAddTrip,
+  StyledImageAddTrip,
+  StyledStartDateAddTrip,
+  StyledTitleAddTrip,
+} from "./TripForm.styled";
+
 import { useRouter } from "next/router";
 
 export default function TripForm() {
@@ -24,58 +35,75 @@ export default function TripForm() {
   }
 
   return (
-    <>
+    <StyledFormAddTrip>
       <form onSubmit={handleSubmit}>
         <fieldset>
-          <label htmlFor="title">Title (max. 30 characters)*</label>
-          <input id="title" name="title" type="text" maxLength="30" required />
+          <StyledTitleAddTrip>
+            <label htmlFor="title">Title (max. 30 characters)*</label>
+            <input
+              id="title"
+              name="title"
+              type="text"
+              maxLength="30"
+              required
+            />
+          </StyledTitleAddTrip>
+          <StyledStartDateAddTrip>
+            <label htmlFor="startDate">Starting date (dd/mm/yyyy)*</label>
+            <input id="startDate" name="startDate" type="date" required />
+          </StyledStartDateAddTrip>
+          <StyledEndDateAddTrip>
+            <label htmlFor="endDate">Ending date (dd/mm/yyyy)*</label>
+            <input id="endDate" name="endDate" type="date" required />
+          </StyledEndDateAddTrip>
 
-          <label htmlFor="startDate">Starting date (dd/mm/yyyy)*</label>
-          <input id="startDate" name="startDate" type="date" required />
+          <StyledCityAddTrip>
+            <label htmlFor="city">City (max. 30 characters)*</label>
+            <input id="city" name="city" type="text" maxLength="30" required />
+          </StyledCityAddTrip>
 
-          <label htmlFor="endDate">Ending date (dd/mm/yyyy)*</label>
-          <input id="endDate" name="endDate" type="date" required />
+          <StyledCountryAddTrip>
+            <label htmlFor="country">Country (max. 30 characters)*</label>
+            <input
+              id="country"
+              name="country"
+              type="text"
+              maxLength="30"
+              required
+            />
+          </StyledCountryAddTrip>
 
-          <label htmlFor="city">City (max. 30 characters)*</label>
-          <input id="city" name="city" type="text" maxLength="30" required />
+          <StyledImageAddTrip>
+            <label htmlFor="image">Image (URL)*</label>
+            <input
+              id="image"
+              name="image"
+              type="text"
+              placeholder="For example www.my-image.com"
+              required
+            />
+          </StyledImageAddTrip>
 
-          <label htmlFor="country">Country (max. 30 characters)*</label>
-          <input
-            id="country"
-            name="country"
-            type="text"
-            maxLength="30"
-            required
-          />
-
-          <label htmlFor="image">Image (URL)*</label>
-          <input
-            id="image"
-            name="image"
-            type="text"
-            placeholder="For example www.my-image.com"
-            required
-          />
-
-          <label htmlFor="description">
-            Description (max. 150 characters)*
-          </label>
-          <textarea
-            rows="8"
-            cols="30"
-            maxLength="150"
-            id="description"
-            name="description"
-            type="text"
-            required
-            placeholder="Enter your description"
-          ></textarea>
-          <p>150 characters left</p>
-
+          <StyledDescriptionAddTrip>
+            <label htmlFor="description">
+              Description (max. 150 characters)*
+            </label>
+            <textarea
+              rows="8"
+              cols="30"
+              maxLength="150"
+              id="description"
+              name="description"
+              type="text"
+              required
+              placeholder="Enter your description"
+            ></textarea>
+            <p>150 characters left</p>
+          </StyledDescriptionAddTrip>
           <button type="submit">+ Add Trip</button>
         </fieldset>
         <p>* required form field</p>
       </form>
-    </>
+    </StyledFormAddTrip>
   );
 }
