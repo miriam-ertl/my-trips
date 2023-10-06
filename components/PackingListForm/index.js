7f32bd19fc83180089758ee60252052c1793297b
Bonnie Baumeister <bonnie.hoika@gmx.de>
2023-10-06 12:17:20 +0200

    refactor: move StxledPackingListForm to PackingListForm.styled.js

--- components/PackingListForm/index.js
+++ components/PackingListForm/index.js
@@ -1,22 +1,7 @@
-import styled from "styled-components";
-
-const StyledPackingListForm = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  border-bottom-style: dashed;
-  border-width: 2px;
-  flex-wrap: nowrap;
-  padding: 5px;
-  align-items: baseline;
-`;
-const StyledAddItemButton = styled.button`
-  width: 50px;
-  height: 20px;
-  margin: 2px 2px 2px 5px;
-  display: flex;
-  justify-content: center;
-`;
+import {
+  StyledAddItemButton,
+  StyledPackingListForm,
+} from "./PackingListForm.styled";
 
 export default function PackingListForm({ onHandleAddToPackingList }) {
   async function handleSubmit(event) {
