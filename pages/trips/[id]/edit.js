da15a40a36a91665d9842ce15f459773d744ecda
David Krauss <davey.krauss@googlemail.com>
2023-10-12 19:38:52 +0200

    feat: enable editing picture in editForm

--- pages/trips/[id]/edit.js
+++ pages/trips/[id]/edit.js
@@ -7,6 +7,24 @@ import { useState } from "react";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
+const CLOUD_NAME = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
+const UPLOAD_PRESET = "ml_default";
+
+async function uploadImage(file) {
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", UPLOAD_PRESET);
+  const response = await fetch(
+    `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+  const { url, width, height } = await response.json();
+  return { url, width, height };
+}
+
 export default function EditTrip() {
   const { mutate } = useSWR("/api/trips");
   const router = useRouter();
@@ -26,7 +44,10 @@ export default function EditTrip() {
 
   async function handleEdit(event) {
     event.preventDefault();
-
+    let image;
+    if (showFileInput) {
+      image = await uploadImage(event.target.image.files[0]);
+    }
     const formData = new FormData(event.target);
     const tripData = Object.fromEntries(formData);
     const response = await fetch(`/api/trips/${id}`, {
@@ -34,7 +55,7 @@ export default function EditTrip() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(tripData),
+      body: JSON.stringify({ ...tripData, image }),
     });
 
     if (response.ok) {
@@ -111,14 +132,7 @@ export default function EditTrip() {
           {showFileInput ? (
             <>
               <label htmlFor="image">Image (URL)*</label>
-              <input
-                id="image"
-                name="image"
-                type="file"
-                //      placeholder="For example www.my-image.com"
-                required
-                //defaultValue={trip.image.url}
-              ></input>
+              <input id="image" name="image" type="file" required></input>
             </>
           ) : (
             <button type="button" onClick={() => setShowFileInput(true)}>
